cd31866be1c0d0d67beda27131b754666e2160bf
Christopher D Couture <c.couture@exsi.ca>
2022-05-25 04:18:30 -0400

    added home page with routing

---
+++ personal.js
@@ -0,0 +1,31 @@
+$.getJSON("./index.json",
+	function (json) {
+		const { items } = json;
+		const userData = { ...items };
+		appendItem(userData);
+	}
+);
+
+function GetItem(item) {
+	const itemContainer = document.createElement("div");
+	itemContainer.classList.add("item");
+	itemContainer.innerHTML += `<div class="item-name" id="${item.fullName.includes("Rune") ? "RUNE" : item.quality}">${item.fullName}</div>`;
+	if (item.ethereal) {
+		itemContainer.innerHTML += `<div class="eth" id="ETHEREAL">Ethereal</div>`;
+	}
+	item.affixes.map((affix) => {
+		itemContainer.innerHTML += `<div class="stat">${affix.name}: ${affix.value}</div>`;
+	});
+	return itemContainer;
+}
+
+function appendItem(userData) {
+	const main = document.getElementById("root");
+	main.innerHTML = "";
+
+	if (userData.personalStash.length > 0) {
+		userData.personalStash.map((item) => {
+			main.appendChild(GetItem(item));
+		});
+	}
+}
